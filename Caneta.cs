752ffac8dd4b4b1d61930d4e8c5ff89391c4e9b7
Cup-Grape <feliposs11@gmail.com>
2021-02-19 19:21:16 -0300

    Atualização

--- Caneta.cs
+++ Caneta.cs
@@ -1,37 +1,73 @@
 using System;
 
+//Estrutura do objeto//
 class Caneta
 {
+
+    //Atributos//
     public double ponta;
     public string cor;
-    public string tampa;
-    protected int tinta;
+    public bool tampa = true;
+    protected int tintaQtde;
+    //Atributos//
 
-    public void rabiscar(int tinta, string cor)
+
+    //Métodos//
+    public void rabiscar(int tintaQtde)
     {
-        this.tinta = tinta;
-        this.cor = cor;
-        if(tinta >= 1)
-        {
-            Console.WriteLine($"A tinta está {tinta}%");
-            Console.WriteLine($"Rabiscando, cor {cor}");
-            Console.ReadLine();
-        }
+        this.tintaQtde = tintaQtde;
         
+        if(tampa == false)
+        {
+            if(tintaQtde >= 1)
+            {
+                Console.WriteLine($"A tinta está {tintaQtde}%");
+                Console.WriteLine("A caneta está rabiscando..");
+                Console.ReadLine();
+            }
+                
+            else
+            {
+                Console.WriteLine("Tinta insuficiente!");
+                Console.WriteLine($"Nivel da tinta: {tintaQtde}");
+                Console.ReadLine();
+            }
+
+        }
         else
         {
-            Console.WriteLine("Tinta insuficiente!");
-            Console.WriteLine($"Nivel da tinta: {tinta}");
-            Console.ReadLine();
+            Console.WriteLine("A caneta está tampada");
+            Console.WriteLine("Não é possivel rabiscar");
         }
-
     }
+
 
     public void tamparEdestampar()
     {
+        int escolha;
+        Console.WriteLine("Tampa da caneta: ");
+        Console.WriteLine("(1) Tampado");
+        Console.WriteLine("(2) Destampada");
+        escolha = Convert.ToInt32(Console.ReadLine());
 
+        if(escolha == 1)
+        {
+            tampa = true;
+            Console.WriteLine("Caneta tampada");
+        }
+
+        if(escolha ==2)
+        {
+            tampa = false;
+            Console.WriteLine("Destampada");
+        }
+
+        else
+        {
+            Console.WriteLine("Não conheço esse comando!");
+        }
     }
-
-
+    //Métodos//
 
 }
+//Estrutura do objeto//
--- Program.cs
+++ Program.cs
@@ -8,12 +8,25 @@ namespace P6exCaneta
         {
             int nv;
             string cr;
+            double pt;
+
             Caneta obj = new Caneta();
+
             Console.Write("Nivel da tinta:");
             nv = Convert.ToInt32(Console.ReadLine());
             Console.Write("Cor da tinta:");
             cr = Console.ReadLine();
-            obj.rabiscar(nv, cr);
+            Console.Write("Espessura da ponta: ");
+            pt = Convert.ToDouble(Console.ReadLine());
+
+            obj.cor = cr;
+            obj.ponta = pt;
+
+            Console.WriteLine($"Ponta: {cr}  Ponta: {pt}");
+
+            obj.tamparEdestampar();
+            obj.rabiscar(nv);
+            
 
         }
     }
